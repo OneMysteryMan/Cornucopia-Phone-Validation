c791a88890d30cd6fbd31511bea8d5aba893c66c
Yirri Cruz <YirriCruz@outlook.com>
2023-03-11 14:15:34 +1000

    Add region names/code map

---
+++ Regions.cs
@@ -0,0 +1,31 @@
+﻿using PhoneNumbers;
+
+using System.Collections.Immutable;
+
+namespace Cornucopia_Phone_Validation;
+
+internal static class RegionMap
+{
+	internal static IResult Json = GetRegionMapJson();
+
+	internal static IResult GetRegionMapJson()
+	{
+		var regionDict = LocaleData.Data.Keys
+			.Select(code => (name: GetCountryName(code), code))
+			.OrderBy(tuple => tuple.name)
+			.ToDictionary(tuple => tuple.name, tuple => tuple.code);
+		return Results.Json(regionDict);
+	}
+
+	private static string GetCountryName(string code)
+	{
+		ImmutableDictionary<string, string> langNames = LocaleData.Data[code];
+		if (!langNames.TryGetValue("en", out var name))
+			return "";
+
+		if (name.Length > 0 && name[0] == '*')
+			return langNames[name[1..]];
+
+		return name;
+	}
+}
