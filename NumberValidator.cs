73cca2c64777299a6196220509fa3f08b4a6c8f0
Yirri Cruz <YirriCruz@outlook.com>
2023-03-11 14:16:07 +1000

    Add validate phone number

---
+++ NumberValidator.cs
@@ -0,0 +1,50 @@
+﻿using PhoneNumbers;
+
+namespace Cornucopia_Phone_Validation;
+
+public class NumberValidator
+{
+	internal static IResult Parse(string? phoneNumber, string? region)
+	{
+		if (phoneNumber is null) return Results.BadRequest("Missing phoneNumber!");
+		if (region is null) return Results.BadRequest("Missing region!");
+
+		try
+		{
+			var util = PhoneNumberUtil.GetInstance();
+			var number = util.Parse(phoneNumber, region);
+			var isValid = util.IsValidNumber(number);
+			return Results.Json(new ValidateResult
+			{
+				IsValid = isValid,
+				IsPossible = util.IsPossibleNumber(number),
+				PhoneType = TypeAsString(util.GetNumberType(number)),
+				InternationalFormat = util.Format(number, PhoneNumberFormat.INTERNATIONAL)
+			});
+		}
+		catch (Exception)
+		{
+			return Results.Json(new ValidateResult
+			{
+				IsValid = false,
+				IsPossible = false,
+				PhoneType = TypeAsString(PhoneNumberType.UNKNOWN),
+				InternationalFormat = ""
+			});
+		}
+	}
+
+	private static string TypeAsString(PhoneNumberType type)
+	{
+		var text = type.ToString().ToLower().Replace("_", " ").AsSpan();
+		return string.Concat(text[0].ToString().ToUpper(), text[1..]);
+	}
+
+	internal readonly struct ValidateResult
+	{
+		public bool IsValid { get; init; }
+		public bool IsPossible { get; init; }
+		public string PhoneType { get; init; }
+		public string InternationalFormat { get; init; }
+	}
+}
