b9b0be73dfa202b01e1e964e5b5e11b40ff327a5
Yirri Cruz <YirriCruz@outlook.com>
2023-03-11 14:16:38 +1000

    Add API routes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
+using Cornucopia_Phone_Validation;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.MapGet("/api/validate", (string phoneNumber) =>
-{
-	return phoneNumber;
-});
+app.MapGet("/api/regions", () => RegionMap.Json);
+app.MapGet("/api/validate", NumberValidator.Parse);
 
 app.Run();
